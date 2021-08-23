fdced2503c59d6fd67b285d18d8d9d66ffeb53dd
gravy johnson <gravy@gravyjoneslocker.uk>
2021-08-23 16:48:33 +0100

    Minor setting changes

--- locker/settings.py
+++ locker/settings.py
@@ -118,13 +118,11 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_FILES_DIRS = [
-    os.path.join(BASE_DIR, x, 'static') for x in CUSTOM_APPS
+STATICFILES_DIRS = [
+    (x, os.path.join(BASE_DIR, x, 'static')) for x in CUSTOM_APPS
 ]
