30303dedf438bd8298b53ca067d8dde4e92176f5
gravy-jones-locker <gravy@gravyjoneslocker.uk>
2022-02-22 19:19:58 +0000

    v 1.0.0

--- display/static/scripts.js
+++ display/static/scripts.js
@@ -2,41 +2,87 @@ var content = document.getElementById('content');
 var wrapper = document.getElementById('wrapper');
 var navvy = document.getElementById('navvy');
 var logo = document.getElementById('logo');
+var sectionHeader = document.getElementById('section-header');
+var entryHeader = document.getElementById('entry-header');
+var sidebar = document.getElementById('sidebar');
+var technical = document.getElementsByClassName('technical');
+var feedback = document.getElementsByClassName('feedback');
+var compile = document.getElementsByClassName('compile');
+var container = document.getElementById('container');
+var entries = document.getElementsByClassName('section');
+
+var activeEntry = undefined;
 var activeImg = undefined;
-var activeHeader = undefined;
 
-content.addEventListener('activate.bs.scrollspy', processScroll);
+content.addEventListener('scroll', processScroll);
+window.addEventListener('resize', processResize);
 
-function processScroll() {
+processResize();
 
-    // Hide the previously active image/header pair
+function processScroll(event) {
+    marginTop = entries[0].offsetTop;
+    for (var entry of entries) {
+        if (entry == activeEntry) {
+            continue;
+        };
+        delta_y = Math.abs(content.scrollTop - entry.offsetTop + marginTop);
+        if (delta_y < 200) {
+            if (entry == activeEntry) {
+                return;
+            };
+            activeEntry = entry;
+            setFormatting();
+        };
+    };
+};
+
+function setFormatting() {
     if (activeImg != undefined) {
         activeImg.style.visibility = 'hidden';
     };
-    if (activeHeader != undefined) {
-        activeHeader.style.visibility = 'hidden';
-    };
-
-    // Get the currently focused element
-    scrollSpy = bootstrap.ScrollSpy.getInstance(content);
-    elem = document.getElementById(scrollSpy._activeTarget.slice(1));
-
-
-    // Set and show the corresponding image
-    if (elem.id != 'summary') {
-        activeImg = elem.getElementsByClassName('background')[0];
+    if (activeEntry.id != 'summary') {
+        activeImg = activeEntry.getElementsByClassName('background')[0];
         activeImg.style.visibility = 'visible';
     };
+    details = activeEntry.getAttribute('alt').split(' // ');
 
-    // Set and show the corresponding header
-    activeHeader = document.getElementById('subheading__'.concat(elem.id));
-    activeHeader.style.visibility = 'visible';
+    sectionHeader.textContent = details[0];
+    entryHeader.textContent = '// '.concat(details[1]);
+    console.log(activeEntry.offsetTop);
+    content.scrollTop = activeEntry.offsetTop - entries[0].offsetTop;
 
-    // Set and rewrite the corresponding color settings
-    color = elem.getElementsByClassName('color')[0].getAttribute('value');
+    color = activeEntry.getElementsByClassName('color')[0].getAttribute('value');
+    
     wrapper.style.background = color;
     navvy.style.background = color;
 
-    logoColor = elem.getElementsByClassName('logo-color')[0].getAttribute('value');
+    logoColor = activeEntry.getElementsByClassName('logo-color')[0].getAttribute('value');
     logo.style.background = logoColor;
-}
+};
+
+function processResize() {
+    if (window.innerWidth < 1200) {
+        sidebar.style.visibility = 'hidden';
+        sidebar.style.width = 0;
+        sidebar.style.margin = '0px 0px 0px 0px';
+    } else {
+        sidebar.removeAttribute('style');
+    };
+    if (window.innerWidth < 600) {
+        for (var entry of compile) {
+            entry.style.display = 'inline';
+            entry.style.paddingLeft = 0;
+        };
+    } else {
+        for (var entry of compile) {
+            entry.removeAttribute('style');
+        }
+    }
+    if (window.innerWidth < 800) {
+        wrapper.style.padding = '30px 3vw 0px 3vw';
+        container.style.padding = '0 0 0 0';
+    } else {
+        wrapper.removeAttribute('style');
+        container.removeAttribute('style');
+    };
+};
