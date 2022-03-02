fd312f878d2de754fb1468dd63df1b5d66870786
gravy <gravy@gravyjoneslocker.uk>
2022-03-02 17:26:58 +0000

    Fully featured dynamic version

--- display/static/scripts.js
+++ display/static/scripts.js
@@ -1,116 +1,138 @@
 var content = document.getElementById('content');
 var wrapper = document.getElementById('wrapper');
-var navvy = document.getElementById('navvy');
 var logo = document.getElementById('logo');
 var sectionHeader = document.getElementById('section-header');
 var entryHeader = document.getElementById('entry-header');
-var sidebar = document.getElementById('sidebar');
-var technical = document.getElementsByClassName('technical');
-var feedback = document.getElementsByClassName('feedback');
-var compile = document.getElementsByClassName('compile');
 var container = document.getElementById('container');
+var navvy = document.getElementById('navvy');
+var sidebar = document.getElementById('sidebar');
 var entries = document.getElementsByClassName('section');
-var contactLogo = document.getElementById('contact-logo');
-var contactLogoIcons = document.getElementsByClassName('contact-icon');
+var links = document.getElementsByClassName('nav-link');
+
+var mobileWidth = 1200;
+var windowWidth = 0;  // Just a placeholder value
 
 var activeEntry = entries[0];
 var activeEntryIndex = 0;
 var activeImg = undefined;
-var scrollTop = 0;
-var deltaY = 0;
-var windowWidth = 0;
 
+var contentScrollTop = 0;
 var containerScrollTop = 0;
-var containerDeltaY = 0;
 
-content.addEventListener('scroll', processScroll);
-container.addEventListener('scroll', function (event) {
 
+function processContainerScroll() {
+
+    if (window.innerWidth > mobileWidth) {
+        return;
+    }
+
+    // Get the scroll direction and reset static var for next time
     containerDeltaY = container.scrollTop - containerScrollTop;
     containerScrollTop = container.scrollTop;
 
+    // Then check that the relevant settings are not already configured
+    if (container.style.overflowY == 'hidden') {
+        return;
+    };
+
+    // If scrolling down and < 20px distance to top of wrapper then change the scroll focus
     if (containerDeltaY > 0 && container.scrollTop + 20 >= wrapper.offsetTop) {
         content.style.overflowY = 'scroll';
         container.style.overflowY = 'hidden';
+
+        // Set the position of the two scrollable containers going down
         container.scrollTop = wrapper.offsetTop;
         content.scrollTop = 1;
     };
-});
-window.addEventListener('resize', processResize);
+};
 
-processResize();
+function processContentScroll() {
 
-function processScroll(event) {
+    // Work out the scroll direction
+    deltaY = content.scrollTop - contentScrollTop;
+    contentScrollTop = content.scrollTop;
+
+    if (windowWidth < mobileWidth) {
+        checkContentScrollFocus(deltaY);
+    };
+    checkEntryFocus(deltaY);
+};
+
+function checkContentScrollFocus(deltaY) {
+
+    // First check that the relevant settings are not already configured
+    if (content.style.overflowY == 'hidden') {
+        return;
+    };
+
+    // If scrolling up and at top of content then change the scroll focus
+    if (deltaY < 0 && activeEntryIndex == 0 && content.scrollTop <= 10) {
+        content.style.overflowY = 'hidden';
+        container.style.overflowY = 'scroll';
+
+        // Set the starting position of the two scrollable containers going up
+        content.scrollTop = 0;
+
+        scrollPos = sidebar.offsetHeight - 1;
+        container.scrollTop = scrollPos;
+    };
+};
+
+function checkEntryFocus(deltaY) {
 
     // Set starting defaults
     changedFocus = false;
     marginTop = entries[0].offsetTop;
 
-    // Work out the scroll direction
-    deltaY = content.scrollTop - scrollTop;
-    scrollTop = content.scrollTop;
-
-    if (deltaY < 0 && activeEntryIndex == 0 && content.scrollTop <= 10) {
-        content.style.overflowY = 'hidden';
-        container.style.overflowY = 'scroll';
-        content.scrollTop = 0;
-        container.scrollTop = wrapper.offsetTop - 13;
-    };
-
-    if (deltaY == 0) {
-        return;
-    } 
-    if (deltaY >= 0) {
-        // If moving down the page then look for distance to next
+    // If scrolling down then look for distance to next
+    if (deltaY > 0) {
         refIndex = activeEntryIndex + 1;
-        if (refIndex > entries.length - 1) {
-            return;
-        };
-        ref = entries[refIndex];
+        if (refIndex < entries.length) {
+            ref = entries[refIndex];
 
-        distRefEntry = content.scrollTop - ref.offsetTop + marginTop;
-        if (distRefEntry > -100) {
-            changedFocus = true;
+            distRefEntry = content.scrollTop - ref.offsetTop + marginTop;
+            if (distRefEntry > -100) {
+                changedFocus = true;
+            };
         };
-    } else {
-        // Otherwise look for distance to previous
+
+    // Otherwise look for distance to previous
+    } else if (deltaY < 0) {
         refIndex = activeEntryIndex - 1;
-        if (refIndex < 0) {
-            return;
-        }
-        ref = entries[refIndex];
-        offsetBottom = ref.offsetTop + ref.offsetHeight;
+        if (refIndex > -1) {
+            ref = entries[refIndex];
+            offsetBottom = ref.offsetTop + ref.offsetHeight;
 
-        distRefEntry = content.scrollTop - offsetBottom + marginTop;
-        if (distRefEntry < -100) {
-            changedFocus = true;
-        };
-    };
+            distRefEntry = content.scrollTop - offsetBottom + marginTop;
+            if (distRefEntry < -200) {
+                changedFocus = true;
+            };
+    }};
     if (changedFocus) {
         activeEntry = ref;
         activeEntryIndex = refIndex;
-        setFormatting();
+        setFormatting();  // Use the updated 'active' variables to change the formatting
     };
 };
 
 function setFormatting() {
+
     if (activeImg != undefined) {
-        activeImg.style.visibility = 'hidden';
+        activeImg.style.visibility = 'hidden';  // Hide the existing background
     };
     if (activeEntry.id != 'summary') {
+        // And show the new one
         activeImg = activeEntry.getElementsByClassName('background')[0];
         activeImg.style.visibility = 'visible';
     };
-    details = activeEntry.getAttribute('alt').split(' // ');
 
+    // Use the data encoded in the entry 'alt' attribute to set the header
+    details = activeEntry.getAttribute('alt').split(' // ');
     sectionHeader.textContent = details[0];
     entryHeader.textContent = '// '.concat(details[1]);
-    if (deltaY > 0 && window.innerWidth > 1200) {
-        content.scrollTop = activeEntry.offsetTop - entries[0].offsetTop;
-    }
 
+    // Likewise with the color and wrapper/navvy/logo
     color = activeEntry.getElementsByClassName('color')[0].getAttribute('value');
-    
     wrapper.style.background = color;
     navvy.style.background = color;
 
@@ -119,34 +141,39 @@ function setFormatting() {
 };
 
 function processResize() {
-    if (window.innerWidth == windowWidth) {
-        return;
-    };
-    if (window.innerWidth < 1200) {
-        navvy.style.visibility = 'hidden';
-        navvy.style.height = 0;
-        navvy.style.margin = 0;
-        navvy.style.padding = 0;
-        wrapper.style.padding = '20px 3vw 0px 3vw';
-        container.style.flexDirection = 'column';
-        container.style.padding = '10px 10px 0px 10px';
-        container.style.overflowY = 'scroll';
-        content.style.scrollSnapType = 'unset';
-        sidebar.style.width = '100%';
-        sidebar.style.margin = '0';
-        contact.style.width = '20%';
-        contact.style.paddingLeft = '1.45rem';
-        wrapper.style.height = 'calc(100vh - 10px)';
-        content.style.overflowY = 'hidden';
-        logo.style.width = '80%';
-        for (var entry of compile) {
-            entry.style.display = 'inline';
-            entry.style.paddingLeft = 0;
+    if (window.innerWidth != windowWidth) {
+        for (var elem of document.getElementsByClassName('adaptive')) {
+            if (window.innerWidth < mobileWidth) {
+                elem.classList.add('mobile');
+            } else {
+                elem.classList.remove('mobile');
+            };
         };
-    } else {
-        sidebar.removeAttribute('style');
-        wrapper.removeAttribute('style');
-        container.removeAttribute('style');
+        content.setAttribute('data-bs-offset', activeEntry.offsetHeight);
+        windowWidth = window.innerWidth;
     };
-    windowWidth = window.innerWidth;
 };
+
+function processLinkClick(event) {
+    event.preventDefault();
+    id = event.target.getAttribute('href').slice(1);
+    for (var entry of entries) {
+        if (entry.getAttribute('id') == id) {
+            content.scrollTop = entry.offsetTop;
+        };
+    };
+};
+
+// Check to see if a resized window needs to be made to fit mobile
+window.addEventListener('resize', processResize);
+
+// Check for scroll focus changes and/or entry focus changes
+container.addEventListener('scroll', processContainerScroll);
+content.addEventListener('scroll', processContentScroll);
+
+// Scroll to entries rather than linking
+for (var link of links) {
+    link.addEventListener('click', processLinkClick);
+};
+
+processResize();  // Execute on startup
--- display/views.py
+++ display/views.py
@@ -4,7 +4,4 @@ from .models import *
 def index(request):
     context = {
         "sections": Section.objects.order_by('index')}
-    return render(request, 'display/alt_index.html', context)
-
-def old(request):
-    return render(request, 'display/index.html', {})
+    return render(request, 'display/index.html', context)
